dc3eb56c5ca50a1d3aa6fd62bf6c17fee32f2e04
llBABY-SHARKll <hero1512003@gmail.com>
2024-04-09 14:10:45 +0200

    test4

--- v2/RRTbasePy.py
+++ v2/RRTbasePy.py
@@ -154,11 +154,11 @@ class RRTGraph:
     def isFree (self):
         n=self.number_of_nodes()-1
         (x,y)=(self.x[n],self.y[n])
-        half_width = 4 
-        half_height = 4 
+        half_width = 12 
+        half_height = 12 
         start_x = int(x - half_width)
         start_y = int(y - half_height)
-        square_region = self.gray[start_y:start_y+8, start_x:start_x+8]
+        square_region = self.gray[start_y:start_y+24, start_x:start_x+24]
 
         
         if any(pixel != 255 for pixel in square_region.flatten()) :
@@ -175,11 +175,11 @@ class RRTGraph:
             u=i/100
             x=x1*u+x2*(1-u)
             y=y1*u+y2*(1-u)
-            half_width = 4 
-            half_height = 4 
+            half_width = 12 
+            half_height = 12 
             start_x = int(x - half_width)
             start_y = int(y - half_height)
-            square_region = self.gray[start_y:start_y+8, start_x:start_x+8]
+            square_region = self.gray[start_y:start_y+24, start_x:start_x+24]
             if any(pixel != 255 for pixel in square_region.flatten()):
                 return True
         return False
