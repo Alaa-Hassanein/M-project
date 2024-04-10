4d11811916d2c526c262aaf1d10f6b7a54f43010
llBABY-SHARKll <hero1512003@gmail.com>
2024-04-10 18:07:57 +0200

    finish

--- Testing/pppp.py
+++ Testing/pppp.py
@@ -5,72 +5,69 @@ def RCW():
     global directions
     k = list(directions.keys())
     v = list(directions.values())
-    x = v[0]
-    for i in range(len(v) - 1):
-        v[i] = v[i + 1]
-    v[len(v) - 1] = x
-
-    zip(k, v)
-    directions = dict(zip(k, v))
+    v_rotated=[v[-1]]+v[:-1]
+    directions = dict(zip(k, v_rotated))
 
 
 def RCCW():
     global directions
     k = list(directions.keys())
     v = list(directions.values())
-    x = v[len(v) - 1]
-    i = len(v) - 1
-    print(x)
-    while (i > 0):
-        v[i] = v[i - 1]
-        i = i - 1
-    v[0] = x
-
-    zip(k, v)
-    directions = dict(zip(k, v))
+    v_rotated=v[1:]+[v[0]]
+    
+    directions = dict(zip(k, v_rotated))
 
 
 def MoveForward(pos, maze):
     if directions['forward'] == 'N':
         new_pos = (pos[0] - 1, pos[1])
         if 0 <= new_pos[0] < maze.rows and 0 <= new_pos[1] < maze.cols:
+            print (new_pos)
             return new_pos, 'N'
         else:
+            print (new_pos)
             return pos, directions['forward']  # Stay in place if at border
     if directions['forward'] == 'E':
         new_pos = (pos[0], pos[1] + 1)
         if 0 <= new_pos[0] < maze.rows and 0 <= new_pos[1] < maze.cols:
+            print (new_pos)
             return new_pos, 'E'
         else:
+            print (new_pos)
             return pos, directions['forward']  # Stay in place if at border
     if directions['forward'] == 'S':
         new_pos = (pos[0] + 1, pos[1])
         if 0 <= new_pos[0] < maze.rows and 0 <= new_pos[1] < maze.cols:
+            print (new_pos)
             return new_pos, 'S'
         else:
+            print (new_pos)
             return pos, directions['forward']  # Stay in place if at border
     if directions['forward'] == 'W':
         new_pos = (pos[0], pos[1] - 1)
         if 0 <= new_pos[0] < maze.rows and 0 <= new_pos[1] < maze.cols:
+            print (new_pos)
             return new_pos, 'W'
         else:
+            print (new_pos)
             return pos, directions['forward']  # Stay in place if at border
 
 
 def Wall_Follower(F):
     global directions
     directions = {'forward': 'N', 'left': 'W', 'back': 'S', 'right': 'E'}
-    currPos = (1, 1)  # Start at (1, 1)  **assuming valid starting position**
+    currPos = (F.rows,F.cols)  # Start at (1, 1)  **assuming valid starting position**
     path = ''
     while True:
         if currPos == (1, 1):
+            print("gg")
             break
 
         # Check if there's a wall on the left relative to current direction
         if F.maze_map[currPos][directions['left']] == 0:
             if F.maze_map[currPos][directions['forward']] == 0:
                 # Dead end detected! Try turning around (replace with your strategy)
-                RCW()
+                
                 RCW()  # Turn around (alternative action)
             else:
                 currPos, D = MoveForward(currPos, F)
--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-from pyamaze import maze
-m=maze(6,6)
-m.CreateMaze()
-directions={'forward':'N','left':'W','back':'S','right':'E'}
-d = {(1,5): 'val1', 'key2': 'val2', 'key3': 'val3'}
-currPos=(m.rows,m.cols)
-print(currPos)
-if m.maze_map[currPos][directions['left']]== 0:
-    print("yes")
+from pyamaze import maze,agent
+myMaze=maze(6,6)
+myMaze.CreateMaze()
+a=agent(myMaze,shape='arrow')
+
+
+directions = {'forward': 'N', 'left': 'W', 'back': 'S', 'right': 'E'}
+currPos=(myMaze.rows,myMaze.cols)
+print(myMaze.maze_map[currPos][directions['left']])
+
+
+
+myMaze.run()
